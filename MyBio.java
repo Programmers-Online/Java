0ddfc3e4ac0b8ee17110038db6bbaae1977edc53
Dipak Bari <dipakbari4@gmail.com>
2021-01-01 04:18:52 +0530

    Task 1 Solution

---
+++ MyBio.java
@@ -0,0 +1,9 @@
+class MyBio {
+
+    public static void main(String[] arg) {
+
+        System.out.println("Dipak Bari");
+        System.out.println("Listening music and songs and sometimes books..");
+        System.out.println("Java the complete reference");
+    }
+}
