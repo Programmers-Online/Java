950e8e10a50b5a051cd1fb90b13ce9ca1a8fb904
Dipak Bari <dipakbari4@gmail.com>
2021-01-01 04:17:56 +0530

    First Program in Java

---
+++ FirstProgram.java
@@ -0,0 +1,11 @@
+class FirstProgram {
+
+    public static void main(String arg[]) {
+
+        System.out.println("This is my First Program in Java!");
+
+        System.out.println("This is the second line");
+
+        System.out.println("Dipak Bari");
+    }
+}
