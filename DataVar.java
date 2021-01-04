4ae2184a79de33e4de9fe2d6e590fae6d1db4008
Dipak Bari <dipakbari4@gmail.com>
2021-01-05 03:17:17 +0530

    Data Types and variables

---
+++ DataVar.java
@@ -0,0 +1,64 @@
+public class DataVar {
+
+    public static void main(String[] args) {
+        // You have 3 apples
+        // integers
+        // character
+        // Boolean
+        // a variable is a named memory location and a storage unit in Java
+
+        // Integer
+        // byte, short, int, long
+
+        // Decimal
+        // float, double
+
+        // Character
+        // char
+
+        // Boolean
+        // true, false
+
+        // byte => -128 to 127
+        byte b = 10;
+
+        System.out.println("Value of byte b is : " + b);
+
+        // short => -32,768 to 32,767
+        short sh = 32767;
+
+        System.out.println("Value of short sh is : " + sh);
+
+        // int => -2,147,483,648 to 2,147,483,647
+
+        int i = 45684;
+
+        System.out.println("Value of int i is : " + i);
+
+        // long => 9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+
+        long lng = 44546498798L;
+
+        System.out.println("Value of long lng is : " + lng);
+
+        // float => 2 ^ 32
+        float fl = 3.141592654f;
+
+        System.out.println("Value of float fl is : " + fl);
+
+        // double => 2 ^ 64
+        double dbl = 3.141592654f;
+
+        System.out.println("Value of double dbl is : " + dbl);
+
+        // char => single character
+        char ch = 'D';
+
+        System.out.println("Value of char ch is : " + ch);
+
+        // boolean => true, false
+        boolean isMorning = true;
+
+        System.out.println("Value of boolean isMorning is : " + isMorning);
+    }
+}
