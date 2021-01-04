7ad036ed52bdccf09ae19e12f519c1e80562b4f6
Dipak Bari <dipakbari4@gmail.com>
2021-01-05 03:18:01 +0530

    Data Types and variables Demonstration

---
+++ Apple.java
@@ -0,0 +1,12 @@
+public class Apple {
+    public static void main(String[] args) {
+        // You have 3 apples
+
+        // byte noOfApples = 3; // Assigning variable with a new value
+
+        byte noOfApples; // Declares variable
+        noOfApples = 3; // Assigning value using assignment opertor
+
+        System.out.println("You have " + noOfApples + " apples");
+    }
+}
