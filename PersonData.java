89444199a5bf0111f366f5ec9f9041d402f4f3b6
Dipak Bari <dipakbari4@gmail.com>
2021-01-05 04:13:05 +0530

    Task #2 Solution

---
+++ PersonData.java
@@ -0,0 +1,20 @@
+public class PersonData {
+
+    public static void main(String[] args) {
+        char personInitial = 'J';
+        float bodyTemp = 98.6f;
+        byte age = 28;
+        long phoneNumber = 9584568578L;
+        boolean isAlive = true;
+
+        System.out.println("Person Initial is : " + personInitial);
+
+        System.out.println("Body temperature is : " + bodyTemp);
+
+        System.out.println("Age is : " + age);
+
+        System.out.println("Phone number : " + phoneNumber);
+
+        System.out.println("Is alive : " + isAlive);
+    }
+}
