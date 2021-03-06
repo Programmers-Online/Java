b49e9918549bc96d0ea94b398f128e072d65f8bf
Dipak Bari <dipakbari4@gmail.com>
2021-03-06 06:41:30 +0530

    Relational Operator Program

---
+++ RelationOperator.java
@@ -0,0 +1,23 @@
+public class RelationOperator {
+   public static void main(String[] args){
+
+      int a = 6;
+      int b = 7;
+
+// ( == ) => Equal to 
+// ( != ) => Not Equal to
+// ( < ) => Less than
+// ( > ) => Greater than
+// ( <= ) => Less than or equal to
+// ( >= ) => Greater than or equal to
+
+      System.out.println(" a == b " + (a == b));
+      System.out.println(" a != b " + (a != b));
+
+      System.out.println(" a < b " + (a < b));
+      System.out.println(" a > b " + (a > b));
+
+      System.out.println(" a <= b " + (a <= b));
+      System.out.println(" a >= b " + (a >= b));
+   }
+}
