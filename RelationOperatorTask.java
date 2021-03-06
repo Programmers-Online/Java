a9ae6579b36bfd34afd247ca112e651bba659295
Dipak Bari <dipakbari4@gmail.com>
2021-03-06 06:46:01 +0530

    Relational Operator Task

---
+++ RelationOperatorTask.java
@@ -0,0 +1,16 @@
+public class RelationOperatorTask {
+
+   public static void main(String[] args) {
+
+      int a = 8;
+      int b = 9;
+
+      System.out.println("a == b => " + (a == b));
+      System.out.println("a != b => " + (a != b));
+      System.out.println("a < b => " + (a < b));
+      System.out.println("a > b => " + (a > b));
+      System.out.println("a <= b => " + (a <= b));
+      System.out.println("a >= b => " + (a >= b));
+
+   }
+}
